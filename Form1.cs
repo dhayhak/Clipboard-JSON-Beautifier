f4c66ab93d5cc322b04b486ab2d06cb655482491
Dhia Louhichi <dhay06@gmail.com>
2017-05-01 14:43:34 -0400

    Add files via upload

---
+++ Form1.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using JSONBeautify.Form;
+using Newtonsoft.Json;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            HotKeyManager.RegisterHotKey(Keys.B, KeyModifiers.Alt);
+            HotKeyManager.HotKeyPressed += new EventHandler<HotKeyEventArgs>(HotKeyManager_HotKeyPressed);
+        }
+
+        void HotKeyManager_HotKeyPressed(object sender, HotKeyEventArgs e)
+        {
+            if (Clipboard.ContainsText())
+            {
+                var text = Clipboard.GetText();
+
+                try
+                {
+                    dynamic parsedJson = JsonConvert.DeserializeObject(text);
+                    var beauty = JsonConvert.SerializeObject(parsedJson, Formatting.Indented);
+                    Clipboard.SetText(beauty);
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show("Could not format JSON");
+                }
+            }
+        }
+
+        protected override void SetVisibleCore(bool value)
+        {
+            // Quick and dirty to keep the main window invisible      
+            base.SetVisibleCore(false);
+        }
+    }
+}
